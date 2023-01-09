6db33731d0e330ab362b92c642f60f89ab590f04
gabrielagabriela <gabrielaa.slv@gmail.com>
2023-01-09 08:26:07 -0300

    ex2_sem09_

--- src/produtos/controller/produtos.controller.ts
+++ src/produtos/controller/produtos.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { ProductEntity } from '../entities/produto.entity';
+import { ProdutosService } from '../service/produtos.service';
 
 @Controller('produtos')
-export class ProdutosController {}
+export class ProdutosController {
+  constructor(private produtosService: ProdutosService) {}
+  @Get()
+  async obterTodos(): Promise<ProductEntity[]> {
+    try {
+      return await this.produtosService.findAll();
+    } catch (err) {
+      throw new HttpException({ reason: err?.detail }, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
--- src/produtos/service/produtos.service.ts
+++ src/produtos/service/produtos.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ProductEntity } from '../entities/produto.entity';
 
 @Injectable()
-export class ProdutosService {}
+export class ProdutosService {
+  constructor(
+    @Inject('PRODUCTS_REPOSITORY')
+    private productRepository: Repository<ProductEntity>,
+  ) {}
+
+  async findAll(): Promise<ProductEntity[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        resolve(await this.productRepository.find());
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
