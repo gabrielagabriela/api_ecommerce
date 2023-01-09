195f6a3f6a5de1ed65670557c71bbf0b9cf46227
gabrielagabriela <gabrielaa.slv@gmail.com>
2023-01-09 08:30:43 -0300

    ex03_sem09

--- src/produtos/controller/produtos.controller.ts
+++ src/produtos/controller/produtos.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Res,
+} from '@nestjs/common';
+import { FindProductDTO } from '../dto/find-product.dto';
 import { ProductEntity } from '../entities/produto.entity';
 import { ProdutosService } from '../service/produtos.service';
+import { Response } from 'express';
 
 @Controller('produtos')
 export class ProdutosController {
@@ -11,6 +20,24 @@ export class ProdutosController {
       return await this.produtosService.findAll();
     } catch (err) {
       throw new HttpException({ reason: err?.detail }, HttpStatus.BAD_REQUEST);
+    }
+  }
+  @Get(':id')
+  async obterUm(
+    @Param() params: FindProductDTO,
+    @Res() response: Response,
+  ): Promise<ProductEntity> {
+    try {
+      const founded = await this.produtosService.findOne(params);
+      if (founded) {
+        response.status(HttpStatus.OK).send(founded);
+        return founded;
+      }
+      response
+        .status(HttpStatus.OK)
+        .send(`Nenhum usuário encontrado com o ID ${params.id}`);
+    } catch (error) {
+      throw new HttpException({ reason: error.detail }, HttpStatus.BAD_REQUEST);
     }
   }
 }
--- src/produtos/service/produtos.service.ts
+++ src/produtos/service/produtos.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { FindProductDTO } from '../dto/find-product.dto';
 import { ProductEntity } from '../entities/produto.entity';
 
 @Injectable()
@@ -13,6 +14,15 @@ export class ProdutosService {
     return new Promise(async (resolve, reject) => {
       try {
         resolve(await this.productRepository.find());
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  async findOne(param: FindProductDTO): Promise<ProductEntity> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        resolve(await this.productRepository.findOneBy(param));
       } catch (error) {
         reject(error);
       }
